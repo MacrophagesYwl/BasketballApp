4316135eefdc7eb4931b802bf097a2685871376e
狐白 <1831605011@qq.com>
2021-05-25 21:34:30 +0800

    优化代码

--- app/src/main/java/com/example/basketballapp/MainActivity.java
+++ app/src/main/java/com/example/basketballapp/MainActivity.java
@@ -10,9 +10,9 @@ import com.example.basketballapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
-    private int teamA,teamB,storageA,storageB;
-    private  final  String KEY = "key";
-    private  final String MY_SP = "my_sp",MY_A = "my_A", MY_B = "my_B";
+    private int teamA, teamB, storageA, storageB;
+    private final String KEY = "key";
+    private final String MY_SP = "my_sp", MY_A = "my_A", MY_B = "my_B";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,36 +20,35 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
-        if (savedInstanceState != null)
-        {
+        if (savedInstanceState != null) {
             int[] i = savedInstanceState.getIntArray(KEY);
             teamA = i[0];
             teamB = i[1];
             storageA = i[2];
             storageB = i[3];
-        }else {
+        } else {
             init();
         }
 
         readSP();
         show();
 
-        mBinding.buttonAAdd1.setOnClickListener(v ->{
+        mBinding.buttonAAdd1.setOnClickListener(v -> {
             addA(1);
         });
-        mBinding.buttonAAdd2.setOnClickListener(v ->{
+        mBinding.buttonAAdd2.setOnClickListener(v -> {
             addA(2);
         });
-        mBinding.buttonAAdd3.setOnClickListener(v ->{
+        mBinding.buttonAAdd3.setOnClickListener(v -> {
             addA(3);
         });
-        mBinding.buttonAAdd4.setOnClickListener(v ->{
+        mBinding.buttonAAdd4.setOnClickListener(v -> {
             addB(1);
         });
-        mBinding.buttonAAdd5.setOnClickListener(v ->{
+        mBinding.buttonAAdd5.setOnClickListener(v -> {
             addB(2);
         });
-        mBinding.buttonAAdd6.setOnClickListener(v ->{
+        mBinding.buttonAAdd6.setOnClickListener(v -> {
             addB(3);
         });
         mBinding.imageButtonRevoke.setOnClickListener(v -> {
@@ -59,38 +58,41 @@ public class MainActivity extends AppCompatActivity {
         mBinding.imageButtonReset.setOnClickListener(v -> {
             reset();
         });
-    };
-    public void addA(int i){
+    }
+
+    ;
+
+    public void addA(int i) {
         Storage();
         teamA += i;
         show();
     }
 
-    public void addB(int i){
+    public void addB(int i) {
         Storage();
         teamB += i;
         show();
     }
 
-    public void  show(){
+    public void show() {
         mBinding.textScoreA.setText(String.valueOf(teamA));
         mBinding.textScoreB.setText(String.valueOf(teamB));
     }
 
-    public void revoke(){
+    public void revoke() {
         teamA = storageA;
         teamB = storageB;
         show();
     }
 
-    public void reset(){
+    public void reset() {
         Storage();
         teamB = 0;
         teamA = 0;
         show();
     }
 
-    public void Storage(){
+    public void Storage() {
         storageA = Integer.parseInt(mBinding.textScoreA.getText().toString());
         storageB = Integer.parseInt(mBinding.textScoreB.getText().toString());
     }
@@ -98,10 +100,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putIntArray(KEY,new int[]{teamA,teamB,storageA,storageB});
+        outState.putIntArray(KEY, new int[]{teamA, teamB, storageA, storageB});
     }
 
-    public  void init(){
+    public void init() {
         teamA = 0;
         teamB = 0;
         storageA = 0;
@@ -109,15 +111,15 @@ public class MainActivity extends AppCompatActivity {
         show();
     }
 
-    private void readSP(){
-        SharedPreferences sp = getSharedPreferences(MY_SP,MODE_PRIVATE);
-        teamA = sp.getInt(MY_A,0);
-        teamB = sp.getInt(MY_B,0);
+    private void readSP() {
+        SharedPreferences sp = getSharedPreferences(MY_SP, MODE_PRIVATE);
+        teamA = sp.getInt(MY_A, 0);
+        teamB = sp.getInt(MY_B, 0);
     }
 
     private void writeSP() {
-        SharedPreferences.Editor sp = getSharedPreferences(MY_SP,MODE_PRIVATE).edit();
-        sp.putInt(MY_A,teamA).putInt(MY_B,teamB).apply();
+        SharedPreferences.Editor sp = getSharedPreferences(MY_SP, MODE_PRIVATE).edit();
+        sp.putInt(MY_A, teamA).putInt(MY_B, teamB).apply();
     }
 
     @Override
