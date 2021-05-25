551cfe8413a79721472101dafb843684a556c1a4
狐白 <1831605011@qq.com>
2021-05-25 21:33:22 +0800

    解决了横屏数据丢失的问题和后台关闭数据丢失的问题

--- app/src/main/java/com/example/basketballapp/MainActivity.java
+++ app/src/main/java/com/example/basketballapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.basketballapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.basketballapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
-    private int scroeA,scroeB,saveA,saveB;
+    private int teamA,teamB,storageA,storageB;
+    private  final  String KEY = "key";
+    private  final String MY_SP = "my_sp",MY_A = "my_A", MY_B = "my_B";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
+        if (savedInstanceState != null)
+        {
+            int[] i = savedInstanceState.getIntArray(KEY);
+            teamA = i[0];
+            teamB = i[1];
+            storageA = i[2];
+            storageB = i[3];
+        }else {
+            init();
+        }
+
+        readSP();
         show();
 
         mBinding.buttonAAdd1.setOnClickListener(v ->{
@@ -45,37 +61,69 @@ public class MainActivity extends AppCompatActivity {
         });
     };
     public void addA(int i){
-        save();
-        scroeA += i;
+        Storage();
+        teamA += i;
         show();
     }
 
     public void addB(int i){
-        save();
-        scroeB += i;
+        Storage();
+        teamB += i;
         show();
     }
 
     public void  show(){
-        mBinding.textScoreA.setText(String.valueOf(scroeA));
-        mBinding.textScoreB.setText(String.valueOf(scroeB));
+        mBinding.textScoreA.setText(String.valueOf(teamA));
+        mBinding.textScoreB.setText(String.valueOf(teamB));
     }
 
     public void revoke(){
-        scroeA = saveA;
-        scroeB = saveB;
+        teamA = storageA;
+        teamB = storageB;
         show();
     }
 
     public void reset(){
-        save();
-        scroeB = 0;
-        scroeA = 0;
+        Storage();
+        teamB = 0;
+        teamA = 0;
         show();
     }
 
-    public void save(){
-        saveA = Integer.parseInt(mBinding.textScoreA.getText().toString());
-        saveB = Integer.parseInt(mBinding.textScoreB.getText().toString());
+    public void Storage(){
+        storageA = Integer.parseInt(mBinding.textScoreA.getText().toString());
+        storageB = Integer.parseInt(mBinding.textScoreB.getText().toString());
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray(KEY,new int[]{teamA,teamB,storageA,storageB});
+    }
+
+    public  void init(){
+        teamA = 0;
+        teamB = 0;
+        storageA = 0;
+        storageB = 0;
+        show();
+    }
+
+    private void readSP(){
+        SharedPreferences sp = getSharedPreferences(MY_SP,MODE_PRIVATE);
+        teamA = sp.getInt(MY_A,0);
+        teamB = sp.getInt(MY_B,0);
+    }
+
+    private void writeSP() {
+        SharedPreferences.Editor sp = getSharedPreferences(MY_SP,MODE_PRIVATE).edit();
+        sp.putInt(MY_A,teamA).putInt(MY_B,teamB).apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        writeSP();
+    }
+
 }
